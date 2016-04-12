9104b0704d593f9157990c477808cea63a47d487
Timur Uzel <timurt.uzel@gmail.com>
2016-04-11 22:53:24 -0400

    functional

--- src/toolbar.js
+++ src/toolbar.js
@@ -1,8 +1,6 @@
-export default class toolbar {
-    constructor(location = 'top',
-                width = '20px',
-                buttons = ['hello, toolbar!'],
-                stretch = true) {
-        console.log(location);
-    }
+export default function toolbar(location = 'top',
+                                width = '20px',
+                                buttons = ['hello, toolbar!'],
+                                stretch = true) {
+    console.log(location);
 }
