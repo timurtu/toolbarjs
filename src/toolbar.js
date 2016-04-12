f5261f077d0664fb6db0e8a07657cd0ab9c08dd0
Timur Uzel <timurt.uzel@gmail.com>
2016-04-11 23:08:14 -0400

    another test

--- src/toolbar.js
+++ src/toolbar.js
@@ -1,6 +1,23 @@
 export default function toolbar(location = 'top',
                                 width = '20px',
-                                buttons = ['hello, toolbar!'],
-                                stretch = true) {
-    console.log(location);
+                                buttons = ['hello, toolbar!']) {
+    const toolbar = document.createElement('toolbar');
+
+    toolbar.style.position = 'fixed';
+    // Set location
+    if (location == 'top') {
+
+        styleTop();
+
+    } else if (location == 'bottom') {
+
+    }
+
+}
+
+function styleTop() {
+    toolbar.style.top = '0';
+    toolbar.style.width = '100%';
+    toolbar.style.height = '20px';
+    toolbar.style.backgroundColor = 'red';
 }
---
+++ toolbar.js
@@ -0,0 +1,27 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = toolbar;
+function toolbar() {
+    var location = arguments.length <= 0 || arguments[0] === undefined ? 'top' : arguments[0];
+    var width = arguments.length <= 1 || arguments[1] === undefined ? '20px' : arguments[1];
+    var buttons = arguments.length <= 2 || arguments[2] === undefined ? ['hello, toolbar!'] : arguments[2];
+
+    var toolbar = document.createElement('toolbar');
+
+    toolbar.style.position = 'fixed';
+    // Set location
+    if (location == 'top') {
+
+        styleTop();
+    } else if (location == 'bottom') {}
+}
+
+function styleTop() {
+    toolbar.style.top = '0';
+    toolbar.style.width = '100%';
+    toolbar.style.height = '20px';
+    toolbar.style.backgroundColor = 'red';
+}
