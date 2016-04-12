9fb89baa6f4e4e2d3742070826f4f4155567fe4f
Timur Uzel <timurt.uzel@gmail.com>
2016-04-11 23:30:50 -0400

    upgrade

--- src/toolbar.js
+++ src/toolbar.js
@@ -1,9 +1,15 @@
-export default function toolbar(location = 'top',
+export default function toolbar(color = 'rgba(74, 195, 184, 0.62)',
+                                location = 'top',
                                 width = '50px',
                                 buttons = ['hello, toolbar!']) {
     const toolbar = document.createElement('toolbar');
 
     toolbar.style.position = 'fixed';
+    toolbar.style.margin = '0';
+    toolbar.style.padding = '12px';
+    toolbar.style.boxShadow = '0 2px 1px rgba(0, 0, 0, 0.125)';
+    toolbar.style.backgroundColor = color;
+
     // Set location
     if (location == 'top') {
 
@@ -18,7 +24,6 @@ export default function toolbar(location = 'top',
         toolbar.style.left = '0';
         toolbar.style.width = '100%';
         toolbar.style.height = width;
-        toolbar.style.backgroundColor = 'yellow';
     }
 
     buttons.forEach((button) => {
--- toolbar.js
+++ toolbar.js
@@ -5,13 +5,19 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = toolbar;
 function toolbar() {
-    var location = arguments.length <= 0 || arguments[0] === undefined ? 'top' : arguments[0];
-    var width = arguments.length <= 1 || arguments[1] === undefined ? '50px' : arguments[1];
-    var buttons = arguments.length <= 2 || arguments[2] === undefined ? ['hello, toolbar!'] : arguments[2];
+    var color = arguments.length <= 0 || arguments[0] === undefined ? 'rgba(74, 195, 184, 0.62)' : arguments[0];
+    var location = arguments.length <= 1 || arguments[1] === undefined ? 'top' : arguments[1];
+    var width = arguments.length <= 2 || arguments[2] === undefined ? '50px' : arguments[2];
+    var buttons = arguments.length <= 3 || arguments[3] === undefined ? ['hello, toolbar!'] : arguments[3];
 
     var toolbar = document.createElement('toolbar');
 
     toolbar.style.position = 'fixed';
+    toolbar.style.margin = '0';
+    toolbar.style.padding = '12px';
+    toolbar.style.boxShadow = '0 2px 1px rgba(0, 0, 0, 0.125)';
+    toolbar.style.backgroundColor = color;
+
     // Set location
     if (location == 'top') {
 
@@ -23,7 +29,6 @@ function toolbar() {
         toolbar.style.left = '0';
         toolbar.style.width = '100%';
         toolbar.style.height = width;
-        toolbar.style.backgroundColor = 'yellow';
     }
 
     buttons.forEach(function (button) {
