6463e360d93fba64a829efe0dda45a50220b2962
Timur Uzel <timurt.uzel@gmail.com>
2016-04-11 22:35:07 -0400

    test

---
+++ toolbar.js
@@ -0,0 +1,8 @@
+export default class toolbar {
+    constructor(location = 'top',
+                width = '20px',
+                buttons = ['hello, toolbar!'],
+                stretch = true) {
+        console.log(location);
+    }
+}
