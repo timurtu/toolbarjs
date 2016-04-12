0f11f5d706042aa2bbf1f66dfd426a2b66da191d
Timur Uzel <timurt.uzel@gmail.com>
2016-04-11 22:48:49 -0400

    transpilation

--- toolbar.js
+++ toolbar.js
@@ -1,8 +1,20 @@
-export default class toolbar {
-    constructor(location = 'top',
-                width = '20px',
-                buttons = ['hello, toolbar!'],
-                stretch = true) {
-        console.log(location);
-    }
-}
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var toolbar = function toolbar() {
+    var location = arguments.length <= 0 || arguments[0] === undefined ? 'top' : arguments[0];
+    var width = arguments.length <= 1 || arguments[1] === undefined ? '20px' : arguments[1];
+    var buttons = arguments.length <= 2 || arguments[2] === undefined ? ['hello, toolbar!'] : arguments[2];
+    var stretch = arguments.length <= 3 || arguments[3] === undefined ? true : arguments[3];
+
+    _classCallCheck(this, toolbar);
+
+    console.log(location);
+};
+
+exports.default = toolbar;
