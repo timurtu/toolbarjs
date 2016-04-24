6bfa66b00fcbf1f35edca355c0820d3d5861ad16
Timur Uzel <timurt.uzel@gmail.com>
2016-04-24 16:45:34 -0400

    working on event handling

--- src/toolbar.js
+++ src/toolbar.js
@@ -1,36 +1,45 @@
-export default function toolbar(color = 'rgba(74, 195, 184, 0.62)',
-                                location = 'top',
-                                buttons = ['hello, toolbar!']) {
-    const toolbar = document.createElement('toolbar');
+export default function toolbar(
+  color = 'rgba(74, 195, 184, 0.62)',
+  location = 'top',
+  buttons = ['goodbye, toolbar!']) {
 
-    toolbar.style.position = 'fixed';
-    toolbar.style.margin = '0';
-    toolbar.style.padding = '12px';
-    toolbar.style.boxShadow = '0 2px 1px rgba(0, 0, 0, 0.125)';
-    toolbar.style.backgroundColor = color;
+  const toolbar = document.createElement('toolbar');
 
-    // Set location
-    if (location == 'top') {
+  toolbar.style.position = 'fixed';
+  toolbar.style.margin = '0';
+  toolbar.style.padding = '12px';
+  toolbar.style.boxShadow = '0 2px 1px rgba(0, 0, 0, 0.125)';
+  toolbar.style.backgroundColor = color;
 
-        styleTop();
+  // Set location
+  switch(location) {
+    case 'top':
+      
+      break
+    case 'bottom':
+      break
+  }
+  if (location == 'top') {
 
-    } else if (location == 'bottom') {
+    styleTop();
 
-    }
+  } else if (location == 'bottom') {
 
-    function styleTop() {
-        toolbar.style.top = '0';
-        toolbar.style.left = '0';
-        toolbar.style.width = '100%';
-    }
+  }
 
-    buttons.forEach((button) => {
-        const btnNode = document.createElement('button');
-        btnNode.textContent = button;
-        toolbar.appendChild(btnNode);
-    });
+  function styleTop() {
+    toolbar.style.top = '0';
+    toolbar.style.left = '0';
+    toolbar.style.width = '100%';
+  }
 
-    document.body.appendChild(toolbar);
+  buttons.forEach((button) => {
+    const btnNode = document.createElement('button');
+    btnNode.textContent = button;
+    toolbar.appendChild(btnNode);
+  });
+
+  document.body.appendChild(toolbar);
 
 }
 
--- toolbar.js
+++ toolbar.js
@@ -1,39 +1,47 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 exports.default = toolbar;
 function toolbar() {
-    var color = arguments.length <= 0 || arguments[0] === undefined ? 'rgba(74, 195, 184, 0.62)' : arguments[0];
-    var location = arguments.length <= 1 || arguments[1] === undefined ? 'top' : arguments[1];
-    var buttons = arguments.length <= 2 || arguments[2] === undefined ? ['hello, toolbar!'] : arguments[2];
+  var color = arguments.length <= 0 || arguments[0] === undefined ? 'rgba(74, 195, 184, 0.62)' : arguments[0];
+  var location = arguments.length <= 1 || arguments[1] === undefined ? 'top' : arguments[1];
+  var buttons = arguments.length <= 2 || arguments[2] === undefined ? ['hello, toolbar!'] : arguments[2];
 
-    var toolbar = document.createElement('toolbar');
 
-    toolbar.style.position = 'fixed';
-    toolbar.style.margin = '0';
-    toolbar.style.padding = '12px';
-    toolbar.style.boxShadow = '0 2px 1px rgba(0, 0, 0, 0.125)';
-    toolbar.style.backgroundColor = color;
+  var toolbar = document.createElement('toolbar');
 
-    // Set location
-    if (location == 'top') {
+  toolbar.style.position = 'fixed';
+  toolbar.style.margin = '0';
+  toolbar.style.padding = '12px';
+  toolbar.style.boxShadow = '0 2px 1px rgba(0, 0, 0, 0.125)';
+  toolbar.style.backgroundColor = color;
 
-        styleTop();
-    } else if (location == 'bottom') {}
+  // Set location
+  switch (location) {
+    case 'top':
 
-    function styleTop() {
-        toolbar.style.top = '0';
-        toolbar.style.left = '0';
-        toolbar.style.width = '100%';
-    }
+      break;
+    case 'bottom':
+      break;
+  }
+  if (location == 'top') {
 
-    buttons.forEach(function (button) {
-        var btnNode = document.createElement('button');
-        btnNode.textContent = button;
-        toolbar.appendChild(btnNode);
-    });
+    styleTop();
+  } else if (location == 'bottom') {}
 
-    document.body.appendChild(toolbar);
+  function styleTop() {
+    toolbar.style.top = '0';
+    toolbar.style.left = '0';
+    toolbar.style.width = '100%';
+  }
+
+  buttons.forEach(function (button) {
+    var btnNode = document.createElement('button');
+    btnNode.textContent = button;
+    toolbar.appendChild(btnNode);
+  });
+
+  document.body.appendChild(toolbar);
 }
