afc8fffced2ea5892a84273ad4311cd23b14d5e5
Timur Uzel <timurt.uzel@gmail.com>
2016-04-24 17:21:36 -0400

    click handlers

--- src/toolbar.js
+++ src/toolbar.js
@@ -1,45 +1,53 @@
+const toolbar = document.createElement('toolbar')
+
 export default function toolbar(
   color = 'rgba(74, 195, 184, 0.62)',
   location = 'top',
-  buttons = ['goodbye, toolbar!']) {
+  buttons = [
+    {
+      label: 'goodbye, toolbar!',
+      click: function (event) {
+        console.log(`Clicked on the button with the label ${this.label}. 
+          And the event ${event}.`)
+        }
+      }
+    ]) {
 
-  const toolbar = document.createElement('toolbar');
+  styleToolbar(toolbar)
 
-  toolbar.style.position = 'fixed';
-  toolbar.style.margin = '0';
-  toolbar.style.padding = '12px';
-  toolbar.style.boxShadow = '0 2px 1px rgba(0, 0, 0, 0.125)';
-  toolbar.style.backgroundColor = color;
+  setLocation(location)
 
-  // Set location
-  switch(location) {
+  createButtons(buttons)
+
+  document.body.appendChild(toolbar);
+}
+
+function styleToolbar(toolbar) {
+  toolbar.style.position = 'fixed'
+  toolbar.style.margin = '0'
+  toolbar.style.padding = '12px'
+  toolbar.style.boxShadow = '0 2px 1px rgba(0, 0, 0, 0.125)'
+  toolbar.style.backgroundColor = color
+}
+
+function setLocation(location) {
+  switch (location) {
     case 'top':
+      toolbar.style.top = '0'
+      toolbar.style.left = '0'
+      toolbar.style.width = '100%'
       break
     case 'bottom':
       break
   }
-  if (location == 'top') {
+}
 
-    styleTop();
-
-  } else if (location == 'bottom') {
-
-  }
-
-  function styleTop() {
-    toolbar.style.top = '0';
-    toolbar.style.left = '0';
-    toolbar.style.width = '100%';
-  }
-
+function createButtons(buttons) {
   buttons.forEach((button) => {
-    const btnNode = document.createElement('button');
-    
-    btnNode.textContent = button;
-    toolbar.appendChild(btnNode);
-  });
-
-  document.body.appendChild(toolbar);
-
+    const btnNode = document.createElement('button')
+    btnNode.textContent = button.label
+    btnNode.onclick = event => button.click
+    toolbar.appendChild(btnNode)
+  })
 }
 
